d2bf3faae02187ff3d1ea925cb55436780db902e
Travis Sanchez <Admin@rescomp-13-276293.stanford.edu>
2014-05-07 14:10:25 -0700

    merging with master

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs  = require('express3-handlebars');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var practice = require('./routes/practice_session');
