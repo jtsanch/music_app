a66e469b7b12a4529f37518b58259598e47360af
Travis Sanchez <Admin@rescomp-13-276293.stanford.edu>
2014-05-07 21:42:58 -0700

    adding video synchro

--- public/javascript/practice_session.js
+++ public/javascript/practice_session.js
@@ -6,7 +6,13 @@ $(document).ready(function(){
 
   var session_id  = $("#session_id");
   var if_musician = $("#if_musician");
-
+  
+  var canvas = document.createElement('canvas'); // offscreen canvas.
+  var rafId = null;
+  var startTime = null;
+  var endTime = null;
+  var frames = [];
+  
   //steps... check for fb_instance ref and if chatroom has one peer connection
   //if there is a peer connection
   function start_conversation(){
@@ -38,6 +44,7 @@ $(document).ready(function(){
                 });
               } else {
                 //display error
+
               }
             });
           } else {
@@ -50,6 +57,69 @@ $(document).ready(function(){
     });
   }
 
+  function start_recording(){
+    var ctx = canvas.getContext('2d');
+    var CANVAS_HEIGHT = canvas.height;
+    var CANVAS_WIDTH = canvas.width;
+
+    frames = []; // clear existing frames;
+    startTime = Date.now();
+
+    toggleActivateRecordButton();
+
+    function drawVideoFrame_(time) {
+      rafId = requestAnimationFrame(drawVideoFrame_);
+
+      ctx.drawImage(video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+      var url = canvas.toDataURL('image/webp', 1); // image/jpeg is way faster :(
+      //console.timeEnd('canvas.dataURL() took');
+      frames.push(url); 
+    }
+
+    rafId = requestAnimationFrame(drawVideoFrame_);
+  } 
+
+  function stop_recording() {
+    cancelAnimationFrame(rafId);
+    endTime = Date.now();
+    $('#stop-me').disabled = true;
+    document.title = ORIGINAL_DOC_TITLE;
+
+    toggleActivateRecordButton();
+
+    begin_critique_session();
+
+    console.log('frames captured: ' + frames.length + ' => ' +
+                ((endTime - startTime) / 1000) + 's video');
+
+       
+  }
+
+  function begin_critique_session(){
+    var webmBlob = Whammy.fromImageArray(frames, 1000 / 60);
+    url = window.URL.createObjectURL(webmBlob);
+    $("#critique_video").src = url;      
+
+    if(if_musician){
+      $("#critique_video").src = 
+      render_critique();
+    } else {
+        document.getElementById("#critique_video").addEventListener('loadedmetadata', function() {
+          fb_instance.child('practice_sessions').child(session_id).child('critique_video_time').on('child_changed', function(snapshot){
+            this.currentTime = snapshot.val(); 
+            $("#critique_video").play();
+          });
+          render_critique();
+        }, false);
+    }
+  }
+
+  function render_critique(){
+    //set up firebase listeners for critique
+
+    //
+  }
   navigator.getUserMedia({audio: true, video: true}, function(stream){
       // Set your video displays
       $( '#my-video').prop('src', URL.createObjectURL(stream));
@@ -58,5 +128,4 @@ $(document).ready(function(){
     }, function(){ alert("Camera disabled."); }); 
   }
 
-  
 });
