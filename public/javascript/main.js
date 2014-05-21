32708d4e82861cbef9e30fd9231101b7f5d4a669
Travis Sanchez <Admin@dn0a2258a4.sunet>
2014-05-21 16:03:37 -0700

    Checking master

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -138,7 +138,7 @@ function login_user(){
      
         current_user = JSON.parse(window.localStorage["user"]); 
 
-        var host = location.origin.replace(/^http/, 'wss')
+        var host = location.origin.replace(/^http/, 'wss');
         var ws = new WebSocket(host);
         ws.onopen = function(){
          ws.send(JSON.stringify({func:'user_connect',user_id:current_user.id})); 
